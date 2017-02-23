00d0405651d96ccbd5d791f1a074e3417a25fc8b
Alexis King <lexi.lambda@gmail.com>
2017-02-23 13:26:28 -0800

    Add local login flow

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import '@cjdev/visual-stack/lib/global';
 
-
 import Layout from '@cjdev/visual-stack/lib/layouts/ApplicationLayout';
 import { PageHeader, PageTitle } from '@cjdev/visual-stack/lib/components/PageHeader';
 import { TopNav } from '@cjdev/visual-stack/lib/components/TopNav';
+import PageContent from '@cjdev/visual-stack/lib/components/PageContent';
+import { Button } from '@cjdev/visual-stack/lib/components/Button';
 import { SideNav, Link } from '@cjdev/visual-stack/lib/components/SideNav';
+
+import qs from 'query-string';
 
 const API_URL = process.env.REACT_APP_API_URL;
 const WEBSITE_URL = process.env.REACT_APP_WEBSITE_URL;
@@ -15,32 +18,46 @@ const AUTH_BASE_URL = 'http://staging-io.cjpowered.com';
 const AUTH_URL = `${AUTH_BASE_URL}/auth?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(WEBSITE_URL)}`;
 
 class App extends Component {
-    state = {};
+  state = { loggedIn: false };
 
-    componentDidMount() {
-        fetch('http://localhost:8080/greeting')
-            .then(res => res.json())
-            .then(json => {
-                this.setState({ message: json.message })
-            });
+  componentDidMount() {
+    const hash = qs.parse(window.location.hash);
+    const token = hash.access_token;
+    if (token) {
+      this.setState({ loggedIn: true })
+      fetch('http://localhost:8080/greeting', {
+        headers: { Authorization: `Bearer ${token}` }
+      }).then(res => res.json())
+        .then(json => {
+          this.setState({ message: json.message })
+        });
     }
+  }
 
-    render() {
-        return (
-            <Layout
-                topNav={<TopNav appName="SUMMIT DEMO" />}
-                sideNav={
-                    <SideNav>
-                        <Link><a href="http://www.cj.com">CJ Home</a></Link>
-                    </SideNav>
-                }
-            >
-                <PageHeader>
-                    <PageTitle>{this.state.message}</PageTitle>
-                </PageHeader>
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout
+        topNav={<TopNav appName="SUMMIT DEMO" />}
+        sideNav={
+          <SideNav>
+            <Link><a href="http://www.cj.com">CJ Home</a></Link>
+          </SideNav>
+        }
+      >
+        <PageHeader>
+          <PageTitle>{this.state.message}</PageTitle>
+        </PageHeader>
+        <PageContent>
+          {!this.state.loggedIn &&
+            <Button type="solid-primary" onClick={() => {
+              window.location = `http://staging-io.cjpowered.com/auth?response_type=token&client_id=4znx2dxw7k519xkfmp4jv11cmq&redirect_uri=${encodeURIComponent('http://localhost:3000/')}`;
+            }}>
+              Log In
+            </Button>}
+        </PageContent>
+      </Layout>
+    );
+  }
 }
 
 export default App;
