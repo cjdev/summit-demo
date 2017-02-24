648a317ffc141c3293845632e0d2bdd60405e9f1
Greg Wiley <gwiley@cj.com>
2017-02-24 11:31:41 -0800

    push to s3

---
+++ deploy/publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+content-bucket() {
+  aws cloudformation describe-stacks \
+    --stack-name ${SUMMIT_STACK_NAME} \
+    --query 'Stacks[0].Outputs[?OutputKey==`"ContentStore"`]  | [0].OutputValue' \
+    --output text
+}
+
+aws s3 cp \
+   --recursive \
+   --cache-control 'max-age=31536000' \
+   $(dirname $0)/website/build/ \
+   "s3://$(content-bucket)/"
