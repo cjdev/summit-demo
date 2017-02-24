af8c03c0315ee9d88d6e7d97d0ca3674a280abc4
Alexis King <lexi.lambda@gmail.com>
2017-02-23 16:11:56 -0800

    Initial commit
    Add request to API with CORS

---
+++ api/main.js
@@ -0,0 +1,12 @@
+require('isomorphic-fetch');
+const express = require('express');
+
+var app = express();
+
+app.use(require('cors')());
+
+app.get('/greeting', (req, res) => {
+  res.json({ message: 'Hello, world!' });
+});
+
+app.listen(8080, () => { console.log('listening on port 8080'); });
---
+++ website/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+const API_URL = process.env.REACT_APP_API_URL;
+const WEBSITE_URL = process.env.REACT_APP_WEBSITE_URL;
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+
+const AUTH_BASE_URL = 'http://staging-io.cjpowered.com';
+const AUTH_URL = `${AUTH_BASE_URL}/auth?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(WEBSITE_URL)}`;
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    fetch('http://localhost:8080/greeting')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ message: json.message })
+      });
+  }
+
+  render() {
+    return <h1>{this.state.message}</h1>;
+  }
+}
+
+export default App;
