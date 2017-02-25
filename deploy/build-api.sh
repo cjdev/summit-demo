82a63427df5b353ff785acbaad9b437f29a87424
Alexis King <lexi.lambda@gmail.com>
2017-02-24 17:50:15 -0800

    Fix the build

--- deploy/build-api.sh
+++ deploy/build-api.sh
@@ -3,9 +3,11 @@ set -ex
 
 IMAGE_NAME=114272735376.dkr.ecr.us-east-1.amazonaws.com/summit-demo:"$SUMMIT_STACK_NAME"
 
+echo "prebuild"
+eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
+
 echo "build"
-cd api
-docker build -t "$IMAGE_NAME"
+docker build -t "$IMAGE_NAME" ./api
 
 echo "post build"
 docker push "$IMAGE_NAME"
