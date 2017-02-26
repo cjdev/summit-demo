d92ab64f0062fd7709e1f0a247ee46005206116f
Greg Wiley <gwiley@cj.com>
2017-02-25 16:46:11 -0800

    improve watch

--- provision/watch.sh
+++ provision/watch.sh
@@ -1,6 +1,8 @@
 
 stackname="summit-${1:-demo}"
-watch -g "aws cloudformation describe-stacks --stack-name $stackname | grep StackStatus"
+watch -g \
+  "aws cloudformation describe-stacks" \
+  " --stack-name $stackname --query 'Stacks[0].StackStatus'"
 
 echo -n 'App Site: '
-aws cloudformation describe-stacks --stack-name summit-charlie --query 'Stacks[0].Outputs[?OutputKey==`Link`].OutputValue| @[0]' --output text
+aws cloudformation describe-stacks --stack-name $stackname --query 'Stacks[0].Outputs[?OutputKey==`Link`].OutputValue| @[0]' --output text
