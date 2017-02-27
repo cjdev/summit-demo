771c6a044abcf2648bfed1edf10b39c579972872
Alexis King <lexi.lambda@gmail.com>
2017-02-27 12:58:58 -0800

    we have a cj ui

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,13 @@
 /* eslint-disable */
 import React, { Component } from 'react';
+import '@cjdev/visual-stack/lib/global';
+
+import Layout from '@cjdev/visual-stack/lib/layouts/ApplicationLayout';
+import { PageHeader, PageTitle } from '@cjdev/visual-stack/lib/components/PageHeader';
+import { TopNav } from '@cjdev/visual-stack/lib/components/TopNav';
+import PageContent from '@cjdev/visual-stack/lib/components/PageContent';
+import { Button } from '@cjdev/visual-stack/lib/components/Button';
+import { SideNav, Link } from '@cjdev/visual-stack/lib/components/SideNav';
 
 const API_URL = process.env.REACT_APP_API_URL;
 const WEBSITE_URL = process.env.REACT_APP_WEBSITE_URL;
@@ -20,7 +28,21 @@ class App extends Component {
   }
 
   render() {
-    return <h1>{this.state.message}</h1>;
+    return (
+      <Layout
+        topNav={<TopNav appName="FRUIT LOOPS" />}
+        sideNav={
+          <SideNav>
+            <Link><a href="http://cj.com">CJ Home</a></Link>
+          </SideNav>
+        }
+      >
+      <PageHeader>
+        <PageTitle>{this.state.message}</PageTitle>
+      </PageHeader>
+
+      </Layout>
+    )
   }
 }
 
