8b6ac9c0341249de3cbb06a7f5462096149e4976
Alexis King <lexi.lambda@gmail.com>
2017-02-27 16:39:10 -0800

    Add a simple CJ login

--- website/src/App.js
+++ website/src/App.js
@@ -9,6 +9,8 @@ import PageContent from '@cjdev/visual-stack/lib/components/PageContent';
 import { Button } from '@cjdev/visual-stack/lib/components/Button';
 import { SideNav, Link } from '@cjdev/visual-stack/lib/components/SideNav';
 
+import qs from 'query-string';
+
 const API_URL = process.env.REACT_APP_API_URL;
 const WEBSITE_URL = process.env.REACT_APP_WEBSITE_URL;
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
@@ -20,33 +22,44 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    fetch(`${API_URL}/greeting`)
-      .then(res => res.json())
-      .then(json => {
-        this.setState({ message: json.message })
-      });
+    const hash = qs.parse(window.location.hash);
+    const token = hash.access_token;
+
+    if (token) {
+      this.setState({ loggedIn: true })
+      fetch(`${API_URL}/greeting`, {
+        headers: { Authorization: `Bearer ${token}` }
+      }).then(res => res.json())
+        .then(json => {
+          this.setState(json)
+        });
+    }
   }
 
   render() {
+    return (
+      <Layout
+        topNav={<TopNav appName="GILMORE GIRLS" />}
+        sideNav={
+          <SideNav>
+            <Link><a href="http://www.cj.com">CJ Home</a></Link>
+          </SideNav>
+        }
+      >
+      <PageHeader>
+        <PageTitle>{this.state.message}</PageTitle>
+      </PageHeader>
 
-return (
-    <Layout
-      topNav={<TopNav appName="GILMORE GIRLS" />}
-      sideNav={
-        <SideNav>
-          <Link><a href="http://www.cj.com">CJ Home</a></Link>
-        </SideNav>
-      }
-    >
-    <PageHeader>
-      <PageTitle>{this.state.message}</PageTitle>
-    </PageHeader>
+      <PageContent>
+        <Button type="solid-primary" onClick={() => {
+          window.location = AUTH_URL;
+        }}>
+          Log In
+        </Button>
+      </PageContent>
 
-    <PageContent>
-     </PageContent>
-
-    </Layout>
-  );
+      </Layout>
+    );
   }
 }
 
