a43890f5b27ebbee730ca066b91897694643333d
Alexis King <lexi.lambda@gmail.com>
2017-02-27 17:12:27 -0800

    Fix some issues discovered in dry run 2

--- api/main.js
+++ api/main.js
@@ -9,4 +9,12 @@ app.get('/greeting', (req, res) => {
   res.json({ message: 'Hello, world!' });
 });
 
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ error: 'unauthorized' });
+  } else {
+    next(err);
+  }
+});
+
 app.listen(8080, () => { console.log('listening on port 8080'); });
